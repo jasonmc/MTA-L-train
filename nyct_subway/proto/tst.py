3917d652d95f1a96dd1060b363cd8f853b77eba2
Jason McCandless <me@jasonmc.net>
2014-06-13 00:11:09 -0400

    Code cleanup.

--- nyct_subway/proto/tst.py
+++ nyct_subway/proto/tst.py
@@ -1,4 +1,3 @@
-import txt_pb2
 import gtfs_realtime_pb2
 import urllib2
 from datetime import datetime
@@ -6,42 +5,40 @@ from datetime import datetime
 url = "http://datamine.mta.info/files/k38dkwh992dk/gtfs-l"
 
 
-def getData():
+def getFeedMessage():
+    fm = gtfs_realtime_pb2.FeedMessage()
     response = urllib2.urlopen(url)
     data = response.read()
-    o2.ParseFromString(data)
+    fm.ParseFromString(data)
+    return fm
 
 
-obj = txt_pb2.NyctFeedHeader()
-
-
-o2 = gtfs_realtime_pb2.FeedMessage()
-
-
-getData()
-
-
-def getTrain(i):
+def getTrain(feedMessage, i):
     return list((x.stop_id, datetime.datetime.fromtimestamp(x.departure.time))
-                for x in o2.entity[i].trip_update.stop_time_update)
+                for x in feedMessage.entity[i].trip_update.stop_time_update)
 
 
-def timesForStop(stopId):
-    thisStopTimes = (y for x in o2.entity
-                     for y in x.trip_update.stop_time_update
-                     if y.stop_id == stopId)
-    for j in thisStopTimes:
-        departTime = datetime.fromtimestamp(j.departure.time)
+def timesForStop(feedMessage, stopId):
+    return (datetime.fromtimestamp(y.departure.time)
+            for x in feedMessage.entity
+            for y in x.trip_update.stop_time_update
+            if y.stop_id == stopId)
 
-        # acceptable to err on the side of caution and round down
-        mins = (departTime - datetime.now()).seconds / 60
 
-        print departTime.strftime("%I:%M %p"), ":", mins, "mins"
+def getMinsUntil(departTime):
+    # acceptable to err on the side of caution and round down
+    return (departTime - datetime.now()).seconds / 60
+
+
+def formatIt(departTime, mins):
+    return (departTime.strftime("%I:%M %p") + " : " + str(mins) + " mins")
 
 
 def main():
     jefferson = "L15N"
+    times = timesForStop(getFeedMessage(), jefferson)
+    for x in(formatIt(x, getMinsUntil(x)) for x in times):
+        print x
 
-    timesForStop(jefferson)
-
-main()
+if __name__ == '__main__':
+    main()
